89ccae8f5316214061a1b91f68b9cdad177f5ea8
Anders Lervik <andersle@gmail.com>
2023-09-22 09:34:03 +0200

    Updated docstrings from the common methods for creating objects.

--- turtlemd/inout/common.py
+++ turtlemd/inout/common.py
@@ -1,4 +1,4 @@
-"""Methods for dealing with the creation of objects."""
+"""Methods for creating objects from settings."""
 from __future__ import annotations
 
 import inspect
@@ -20,29 +20,23 @@ def generic_factory(
     registry: dict[str, type],
     name: str = "generic",
 ) -> Any | None:
-    """Create instances of classes based on settings.
+    """Create an instance of a class based on the given settings.
 
-    This method is intended as a semi-generic factory for creating
-    instances of different objects based on simulation input settings.
-    The input settings define what classes should be created and
-    the object_map defines a mapping between settings and the
-    class.
+    Args:
+        settings: Specifies arguments and keyword arguments for object
+            initiation. Must contain a "class" key to specify which
+            class should be instantiated.
+        registry: A mapping from class names to classes.
+        name: Labels the type of classes we create. Mainly
+            used for error messages. Defaults to "generic".
 
-    Parameters
-    ----------
-    settings : dict
-        This defines how we set up and select the order parameter.
-    registry : dict
-        Definitions on how to initiate the different classes.
-    name : string, optional
-        Short name for the object type. Only used for error messages.
+    Returns:
+        An instance of the specified class if successful. Returns
+            None otherwise.
 
-    Returns
-    -------
-    out : instance of a class
-        The created object, in case we were successful. Otherwise we
-        return none.
-
+    Raises:
+        ValueError: If the "class" key is missing in settings or if the
+            specified class is not found in the registry.
     """
     klass_name = settings.get("class", "").lower()
 
@@ -67,19 +61,17 @@ def get_parameter_type(arg: Parameter) -> str:
     This method categorizes the given parameter based on its kind
     (e.g., positional, keyword-only, etc.).
 
-    Parameters:
-    - arg (Parameter): The parameter whose type will be determined.
-      This should be an instance of `inspect.Parameter`.
+    Args:
+        arg: The parameter whose type will be determined.
 
     Returns:
-    - str: The determined type of the parameter. The possible return
-      values are:
-        - "args": for positional parameters or for positional-or-keyword
-          parameters without default values.
-        - "kwargs": for keyword-only parameters or for positional-or-keyword
-          parameters with default values.
-        - "varargs": for variable positional parameters (i.e., *args).
-        - "keywords": for variable keyword parameters (i.e., **kwargs).
+        The type of the parameter. Can be one of the following:
+            - "args": Positional parameters or positional-or-keyword
+                parameters without default values.
+            - "kwargs": Keyword-only parameters or positional-or-keyword
+                parameters with default values.
+            - "varargs": For variable positional parameters (i.e., *args).
+            - "keywords": For variable keyword parameters (i.e., **kwargs).
     """
     if arg.kind == Parameter.POSITIONAL_OR_KEYWORD:
         return "args" if arg.default is Parameter.empty else "kwargs"
@@ -100,23 +92,20 @@ def get_args_and_kwargs(
     This method identifies the required arguments for the constuctor
     of `klass` and retrieves their values from the `settings` dictionary.
 
-    Parameters:
-    - klass (Any): The class for which the constructor arguments
-      need to be extracted.
-    - settings (dict[str, Any]): A dictionary containing potential
-      positional and keyword arguments for `klass.__init__()`.
+    Args:
+        klass: The class for which the constructor arguments need to be
+            extracted.
+        settings: A dictionary containing potential positional and keyword
+            arguments for `klass.__init__()`.
 
     Returns:
-    - tuple:
-        * list: Positional arguments extracted from `settings`.
-        * dict: Keyword arguments extracted from `settings`.
+        A tuple containing:
+            - Positional arguments extracted from `settings`.
+            - Keyword arguments extracted from `settings`.
 
     Raises:
-    - ValueError: If a required positional argument
-      for `klass` is not present in `settings` or if the method needs
-      *args or **kwargs (this since we do not want to make the input
-      file too complex).
-
+        ValueError: If a required positional argument for `klass` is not
+            present in `settings` or if the method needs *args or **kwargs.
     """
     info = inspect_function(klass.__init__)
     used_args, positional_args, keyword_args = set(), [], {}
@@ -167,18 +156,17 @@ def inspect_function(method: Callable) -> dict[str, list[Any]]:
     This function categorizes the parameters of the given function or method
     based on their types (e.g., positional, keyword-only, etc.).
 
-    Parameters:
-    - method (Callable): The function or method to inspect.
+    Args:
+        method: The function or method to inspect.
 
     Returns:
-    - dict: A dictionary categorizing parameters:
-        - "args": Positional parameters or positional-or-keyword parameters
-          without default values.
-        - "kwargs": Keyword-only parameters or positional-or-keyword
-          parameters with default values.
-        - "varargs": Variable positional parameters (*args).
-        - "keywords": Variable keyword parameters (**kwargs).
-
+        A dictionary categorizing parameters:
+            - "args": Positional or positional-or-keyword parameters
+                without default values.
+            - "kwargs": Keyword-only or positional-or-keyword parameters
+                with default values.
+            - "varargs": Variable positional parameters (*args).
+            - "keywords": Variable keyword parameters (**kwargs).
     """
     arguments = inspect.signature(method)
     parameters_by_type = defaultdict(list)
@@ -190,15 +178,14 @@ def inspect_function(method: Callable) -> dict[str, list[Any]]:
 def initiate_instance(klass: type[Any], settings: dict[str, Any]) -> Any:
     """Initialize a class with arguments from settings.
 
-    Parameters:
-    - klass (type[Any]): The class to initialize.
-    - settings (dict[str, Any]): A dictionary containing positional
-      and keyword arguments (if any) for `klass.__init__()`.
+    Args:
+        klass: The class to initialize.
+        settings): A dictionary containing positional and keyword arguments
+            (if any) for `klass.__init__()`.
 
     Returns:
-    - Any: An instance of the given class, initialized with the
-      arguments extracted from `settings`.
-
+        An instance of the given class, initialized with the arguments
+            extracted from `settings`.
     """
     args, kwargs = get_args_and_kwargs(klass, settings)
     msg = 'Initiated "%s" from "%s"'
